28472f735b4ee4515ac0849e1df620e082edfd1d
Rainer2465 <rainerdaniel9@hotmail.com>
2020-02-11 23:35:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,20 +25,19 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static("client/build"));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 require('./app/controller/authController')(app);
 require('./app/controller/projectController')(app);
 
-if (process.env.NODE_ENV === 'production'){
-   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use('/', express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  }); 
 }
 
 
