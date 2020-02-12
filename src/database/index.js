4e57212a0a32fecbfe2cd238f6ff5c84827deec8
Rainer2465 <rainerdaniel9@hotmail.com>
2020-02-11 23:26:51 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://testeadmin:teset0205@cluster0-8k4rz.gcp.mongodb.net/test?retryWrites=true&w=majority',  {
+mongoose.connect(process.env.MONGO_URL || 'mongodb+srv://testeadmin:teset0205@cluster0-8k4rz.gcp.mongodb.net/test?retryWrites=true&w=majority',  {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
